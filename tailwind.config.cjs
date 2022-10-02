a2d6dc9fbe180ddfffc0487cd660ecae47773445
Nolan Lewis <nolan.lewis@dsciphr.com>
2022-10-02 23:36:38 +0200

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "text-green-900",
+    "text-red-900",
+    "bg-green-100",
+    "bg-red-100",
+    "border-green-500",
+    "border-red-500",
+    "todoOpen",
+    "todoDone",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        todoOpen: "rgba(55, 65, 81, 1)",
+        todoDone: "rgba(55, 65, 81, 0.2)",
+      },
+    },
   },
   plugins: [],
 };
